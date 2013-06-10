64c32b8ccea1ba0b8fa5bc9bbaf776dedd7370c1
張鈞豪 <abc50604@yahoo.com.tw>
2013-06-10 20:35:20 +0800

    finish initial UI

---
+++ Poker/src/main/java/com/weigreen/poker/RoomActivity.java
@@ -0,0 +1,16 @@
+package com.weigreen.poker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by roy on 2013/6/10.
+ */
+public class RoomActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
